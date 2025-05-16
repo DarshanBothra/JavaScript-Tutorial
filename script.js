572513638a170ff38e64420ab70c7ed686ee1828
Darshan Bothra <hitmandarshan10@gmail.com>
2025-05-16 18:42:23 +0530

    Node module installs

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Deposit some money
+// 2. Determine the number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine to see if the user won
+// 5. Check if the user won
+// 6. Give the user their winnings/ take their bet
+// 7. Play again 
+
+
